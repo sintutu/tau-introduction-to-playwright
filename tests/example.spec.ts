7d4159f5b6e97883f15ae3bbcbd0a7fcff6adcc0
sintutu <160270685+sintutu@users.noreply.github.com>
2024-03-16 19:22:44 +0200

    test: :test_tube: Chapter 2 exercise

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,27 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('@exercise - can go to Java installation', async ({page}) => {
+  /**
+   * 1. Open the page
+   * 2. Click at Get started
+   * 3. Mouse over the language dropdown
+   * 4. Click at Java
+   * 5. Check the URL
+   * 6. Check the text "Installing Playwright" is not being displayed
+   * 7. Check the below text is being displayed
+   * 
+   * Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.
+   */
+    await page.goto('https://playwright.dev');
+    await page.getByRole('link', { name: 'Get started'}).click();
+    await page.getByRole('button', { name: 'Node.js'}).hover();
+    await page.getByRole('link', { name: 'Java', exact: true}).click();
+    
+    await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+    await expect(page.getByText('Installing Playwright', {exact: true})).not.toBeVisible();
+  
+    const expectedText = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+    await expect(page.getByText(expectedText, {exact: true})).toBeVisible();
+  });
