a15ae6493761c3f030af88107d84209271c1cb35
sintutu <160270685+sintutu@users.noreply.github.com>
2024-03-16 20:55:04 +0200

    feat: :recycle: Use beforeEach and POM

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -3,16 +3,48 @@ import { type Locator, type Page, expect } from '@playwright/test';
 export class HomePage {
     readonly page: Page;
     readonly getStartedButton: Locator;
+
+    readonly languageButton: Locator;
+    readonly javaLanguageLink: Locator;
+    readonly installingPlaywrightText: Locator;
+    readonly playwrightJavaBlurb: Locator;
+
     readonly pageTitle: RegExp;
+
+    readonly expectedText = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
 
     constructor(page: Page) {
         this.page = page;
         this.getStartedButton = page.getByRole('link', { name: 'Get started' });
         this.pageTitle = /Playwright/;
+        this.languageButton = page.getByRole('button', { name: 'Node.js'});
+        this.javaLanguageLink = page.getByRole('link', { name: 'Java', exact: true});
+        this.installingPlaywrightText = page.getByText('Installing Playwright', {exact: true});
+        this.playwrightJavaBlurb = page.getByText(this.expectedText, {exact: true})
     }
 
     async clickGetStarted() {
         await this.getStartedButton.click();
+    }
+
+    async hoverOverLanguageButton() {
+        await this.languageButton.hover();
+    }
+
+    async clickOnJavaLanguageLink() {
+        await this.javaLanguageLink.click();
+    }
+
+    async assertJavaInUrl() {
+        await expect(this.page).toHaveURL('https://playwright.dev/java/docs/intro');
+    }
+
+    async assertInstallingPlaywrightTextNotVisible() {
+        await expect(this.installingPlaywrightText).not.toBeVisible();
+    }
+
+    async assertPlaywrightJavaBlurbIsVisible() {
+        await expect(this.playwrightJavaBlurb).toBeVisible();
     }
 
     async assertPageTitle() {
@@ -21,3 +53,13 @@ export class HomePage {
 }
 
 export default HomePage;
+
+// await homepage.clickGetStarted();
+// await page.getByRole('button', { name: 'Node.js'}).hover();
+// await page.getByRole('link', { name: 'Java', exact: true}).click();
+
+// await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+// await expect(page.getByText('Installing Playwright', {exact: true})).not.toBeVisible();
+
+// const expectedText = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
+// await expect(page.getByText(expectedText, {exact: true})).toBeVisible();
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,42 +1,47 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/home-page'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const URL = 'https://playwright.dev/';
+let homepage:HomePage;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.beforeEach(async ({ page }) => {
+  await page.goto(URL);
+  homepage = new HomePage(page);
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Playwright website', ()=> {
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('@exercise - can go to Java installation', async ({page}) => {
-  /**
-   * 1. Open the page
-   * 2. Click at Get started
-   * 3. Mouse over the language dropdown
-   * 4. Click at Java
-   * 5. Check the URL
-   * 6. Check the text "Installing Playwright" is not being displayed
-   * 7. Check the below text is being displayed
-   * 
-   * Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.
-   */
-    await page.goto('https://playwright.dev');
-    await page.getByRole('link', { name: 'Get started'}).click();
-    await page.getByRole('button', { name: 'Node.js'}).hover();
-    await page.getByRole('link', { name: 'Java', exact: true}).click();
-    
-    await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
-    await expect(page.getByText('Installing Playwright', {exact: true})).not.toBeVisible();
-  
-    const expectedText = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
-    await expect(page.getByText(expectedText, {exact: true})).toBeVisible();
+  test('has title', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
   });
+
+  test('get started link', async ({ page }) => {
+    // Click the get started link.
+    await homepage.clickGetStarted();
+
+    // Expects page to have a heading with the name of Installation.
+    await homepage.assertPageTitle();
+  });
+
+  test('@exercise - can go to Java installation', async ({page}) => {
+    /**
+     * 1. Open the page
+     * 2. Click at Get started
+     * 3. Mouse over the language dropdown
+     * 4. Click at Java
+     * 5. Check the URL
+     * 6. Check the text "Installing Playwright" is not being displayed
+     * 7. Check the below text is being displayed
+     * 
+     * Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.
+     */
+      await homepage.clickGetStarted();
+      await homepage.hoverOverLanguageButton();
+      await homepage.clickOnJavaLanguageLink()
+      
+      await homepage.assertJavaInUrl()
+      await homepage.assertInstallingPlaywrightTextNotVisible();
+      await homepage.assertPlaywrightJavaBlurbIsVisible();
+    });
+});
