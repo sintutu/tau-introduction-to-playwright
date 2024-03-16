007ec882b5cbdefcfb77fcb25f13fd0951f70d93
sintutu <160270685+sintutu@users.noreply.github.com>
2024-03-16 13:50:28 +0200

    fix: :bug: Restore playwright.config.ts after importing playwright@latest
    Restoring the commented out code that was incorrectly removed

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig, devices } from '@playwright/test';
+import baseEnvUrl from './utils/environmentBaseUrl';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -21,6 +22,20 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // reporter: [['html', { open: 'always' }]], //always, never and on-failure (default).
+  // reporter: [['html', { outputFolder: 'my-report' }]], // report is written into the playwright-report folder in the current working directory. override it using the PLAYWRIGHT_HTML_REPORT
+  // reporter: 'dot',
+  // reporter: 'list',
+  /**
+    reporter: [
+      ['list'],
+      ['json', {  outputFile: 'test-results.json' }]
+    ],
+  */
+  /**
+   * custom reports: https://playwright.dev/docs/test-reporters#custom-reporters 
+  */
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,13 +43,30 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // screenshot: 'only-on-failure',
+    // headless: false,
+    // ignoreHTTPSErrors: true,
+    // viewport: { width: 1280, height: 720 },
+    // video: 'on-first-retry',
   },
+    // timeout: 30000, //https://playwright.dev/docs/test-timeouts
+    // expect: {
+      /**
+       * Maximum time expect() should wait for the condition to be met.
+       * For example in `await expect(locator).toHaveText();`
+       */
+      // timeout: 10000,
+    // },
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
+      // viewport: { width: 1280, height: 720 },
     },
 
     {
@@ -45,6 +77,43 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'all-browsers-and-tests',
+      use: { 
+        baseURL: 'https://playwright.dev/',
+         ...devices['Desktop Safari']
+      },
+    },
+
+    {
+      name: 'all-browsers-and-tests',
+      use: { 
+        baseURL: 'https://playwright.dev/',
+         ...devices['Desktop Firefox']
+      },
+    },
+
+    // Example only
+    {
+      name: 'local',
+      use: { 
+        baseURL: baseEnvUrl.local.home,
+      },
+    },
+
+    // Example only
+    {
+      name: 'ci',
+      use: { 
+         baseURL: process.env.CI
+          ? baseEnvUrl.ci.prefix + process.env.GITHUB_REF_NAME + baseEnvUrl.ci.suffix //https://dev-myapp-chapter-2.mydomain.com
+          : baseEnvUrl.staging.home,
+      },
+      /**
+       * GitHub variables: https://docs.github.com/en/actions/learn-github-actions/variables
+       * GitLab variables: https://docs.gitlab.com/ee/ci/variables/predefined_variables.html#predefined-variables-reference
+       */
     },
 
     /* Test against mobile viewports. */
